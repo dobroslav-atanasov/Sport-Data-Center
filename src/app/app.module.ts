3c1063420902f4e49e02ab8ac6a336049a69f261
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-05-22 21:27:01 +0300

    Created structure of the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    AuthModule,
+    AdminModule,
     AppRoutingModule
   ],
   providers: [],
