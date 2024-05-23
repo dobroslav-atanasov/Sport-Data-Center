b69e57fe1cf6b44c4bff5790d31d6d9af963fe60
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-05-23 14:53:15 +0300

    All validations were implemented.

--- src/app/shared/validators/validator.ts
+++ src/app/shared/validators/validator.ts
@@ -10,3 +10,36 @@ export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): V
 
     return password.value === confirmPassword.value ? null : { passwordMatch: true };
 }
+
+export const passwordNonAlphanumericValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.value as string;
+
+    if (!password) {
+        return null;
+    }
+
+    const isValid = /[^a-zA-Z\d\s:]/.test(password);
+    return isValid ? null : { nonAlphanumeric: true };
+}
+
+export const passwordUpperCaseValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.value as string;
+
+    if (!password) {
+        return null;
+    }
+
+    const isValid = /[A-Z]/.test(password);
+    return isValid ? null : { upperCase: true };
+}
+
+export const passwordLowerCaseValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.value as string;
+
+    if (!password) {
+        return null;
+    }
+
+    const isValid = /[a-z]/.test(password);
+    return isValid ? null : { lowerCase: true };
+}
