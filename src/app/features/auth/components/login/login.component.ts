05ebb3cf51a6ca730f4511581ffb813ebb52b5c6
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-05-28 16:09:56 +0300

    Created login form.

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sd-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  serverError: string;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.serverError = '';
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', Validators.required]
+    });
+  }
+
+  get username() {
+    return this.loginForm.controls['username'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  signInHandler() {
+
+  }
 }
