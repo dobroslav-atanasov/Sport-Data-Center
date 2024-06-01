1671c336bab064b4f3645fa207c227d43e5c3ca0
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-06-01 09:14:10 +0300

    Implemented part of the interceptor.

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,5 +1,57 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { AuthService } from '../../features/auth/services/auth.service';
+import { Router } from '@angular/router';
 
-export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+
+export class HttpCoreInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = this.setApiUrl(req);
+    req = this.setHeaders(req);
+
+    return next.handle(req)
+      .pipe(catchError((error) => {
+        if (error instanceof HttpErrorResponse) {
+          if (error?.status === 401 && this.authService.isLoggedIn()) {
+            return this.refreshToken(req, next);
+          } else {
+            return throwError(() => error);
+          }
+        } else {
+          return throwError(() => error);
+        }
+      }));
+  }
+
+  setHeaders(req: HttpRequest<any>): HttpRequest<any> {
+    req = req.clone({
+      setHeaders: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const token = this.authService.getAccessToken();
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
+    return req;
+  }
+
+  setApiUrl(req: HttpRequest<any>): HttpRequest<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  refreshToken(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    throw new Error('Method not implemented.');
+  }
+}
