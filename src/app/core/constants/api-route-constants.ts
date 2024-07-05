0a7c1b5518e22b38cf73b1b8d10c6bd03fcdbd06
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-05 09:11:58 +0300

    Added new api route.

--- src/app/core/constants/api-route-constants.ts
+++ src/app/core/constants/api-route-constants.ts
@@ -1,5 +1,6 @@
 export const ApiRouteConstants = {
     USERS: "users",
     TOKENS_CREATE: "tokens",
-    TOKENS_CREATE_REFRESH_TOKEN: 'tokens/create-refresh-token'
+    TOKENS_CREATE_REFRESH_TOKEN: 'tokens/create-refresh-token',
+    OLYMPIC_GAMES_GAMES_ALL: 'olympicgames/games/all',
 }
